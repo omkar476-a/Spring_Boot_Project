a0315bd38ee098cffab792f1bc7e74c469d45c0f
omkar476-a <omkarirole5557@gmail.com>
2025-07-07 16:04:41 +0530

    Done conf with frontend

--- src/main/java/com/example/project_1/controller/ProductController.java
+++ src/main/java/com/example/project_1/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.project_1.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,8 @@ import com.example.project_1.model.Product;
 import com.example.project_1.service.ProductService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // Allow React frontend // to avoid cross connection 
+
 @RequestMapping("/api")
 public class ProductController {
 	
